b63ffe6188296436dd3e8f1bff01d3e6bf00c9ae
Germain-L <germain.leignel@gmail.com>
2023-05-19 15:49:46 +0200

    added get users/me after login to reload page

--- src/lib/api/auth/login.ts
+++ src/lib/api/auth/login.ts
@@ -1,9 +1,10 @@
 import { PUBLIC_BACKEND } from '$env/static/public';
+import { userStore } from '$lib/stores/user';
 
 export const login = async (email: string, password: string) => {
 	const body = JSON.stringify({
 		email,
-		password,
+		password
 	});
 
 	const response = await fetch(`${PUBLIC_BACKEND}/auth/login`, {
@@ -12,13 +13,27 @@ export const login = async (email: string, password: string) => {
 			'Content-Type': 'application/json'
 		},
 		body,
-		credentials: 'include'   
+		credentials: 'include'
 	});
 
 	// print cookies
 	console.log(response.headers);
 
 	if (response.status === 200) {
+		const userResponse = await fetch(`${PUBLIC_BACKEND}/api/users/me`, {
+			method: 'GET',
+			credentials: 'include'
+		});
+
+		if (!userResponse.ok) {
+			return false;
+		}
+
+		const json = await userResponse.json();
+		const user = json.user;
+
+		userStore.set(user);
+
 		return true;
 	} else {
 		return false;
